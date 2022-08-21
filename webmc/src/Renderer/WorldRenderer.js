b5f7df939ed0ecf5218dd3558c86dada5c2d6c1c
Wu-jiyan <107091865+Wu-jiyan@users.noreply.github.com>
2022-08-21 21:36:50 +0800

    Add files via upload

---
+++ webmc/src/Renderer/WorldRenderer.js
@@ -0,0 +1,54 @@
+import { Render } from "./Render.js";
+import { Camera } from "./Camera.js";
+import { ChunksModule } from "./WorldChunkModule.js";
+import { HighlightSelectedBlock } from "./HighlightSelectedBlock.js";
+
+class WorldRenderer extends Render {
+    constructor(canvas, world = null) {
+        super(canvas);
+        this.fitScreen();
+        new ResizeObserver(async e => {
+            await new Promise(s => setTimeout(s, 0));
+            this.fitScreen();
+        }).observe(canvas);
+        const {ctx} = this;
+        ctx.clearColor(0.62, 0.81, 1.0, 1.0);
+        ctx.clearDepth(1.0);
+        ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT);
+        ctx.enable(ctx.DEPTH_TEST);
+        ctx.depthFunc(ctx.LEQUAL);
+        ctx.enable(ctx.CULL_FACE);
+        ctx.frontFace(ctx.CCW);
+        this.mainCamera = new Camera(this.aspectRatio, { fovy: 60, pitch: -90 * Math.PI / 180, position: [0, 20, 0] });
+        this.addCamera(this.mainCamera);
+        if (world !== null) this.setWorld(world);
+    };
+    setWorld(world) {
+        if ((!world) || world === this.world) return;
+        world.setRenderer(this);
+        this.world = world;
+        this.mainCamera.bindEntity(world.mainPlayer);
+        this.chunksModule = new ChunksModule(world, this);
+        this.blockHighlight = new HighlightSelectedBlock(world, this);
+    };
+    onRender(timestamp, dt) {
+        this.world.update(dt);
+        this.chunksModule.update();
+        const {ctx} = this;
+        ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT);
+        this.chunksModule.draw();
+        this.blockHighlight.draw();
+        ctx.flush();
+    };
+    dispose() {
+        super.dispose();
+        if (!this.world) return;
+        this.chunksModule.dispose();
+        this.blockHighlight.dispose();
+    };
+};
+
+export {
+    WorldRenderer,
+    WorldRenderer as dafault
+};
---
+++ webmc/src/UI/SettingPage.js
@@ -0,0 +1,52 @@
+
+import { Page, pm } from "./Page.js";
+import { World } from "../World/World.js";
+
+// TODO: 使用单个设置单例来同步设置以及持久化
+
+class SettingPage extends Page {
+    static get shortPageID() { return "setting"; };
+    static get templateUrl() { return "src/UI/SettingPage.html"; };
+    constructor() {
+        super();
+        this.worldTerrainBtns = this.shadowRoot.querySelectorAll(".world-terrain");
+        for (let btn of this.worldTerrainBtns) {
+            if (btn.innerHTML == World.config.terrain)
+                btn.setAttribute("disabled", "true");
+            btn.onclick = (e) => {
+                btn.setAttribute("disabled", "");
+                for (let b of this.worldTerrainBtns)
+                    if (b !== btn) b.removeAttribute("disabled");
+                World.config.terrain = btn.innerHTML;
+            };
+        }
+        if (pm.getPageByID("welcome")) {
+            this.homepageBlur = this.shadowRoot.getElementById("homepage-blur");
+            let blurLevel = (window.getComputedStyle(
+                pm.getPageByID("welcome").bgCanvas).filter.replace(/[^\d.]+/g, "") * window.devicePixelRatio).toFixed(1);
+            this.homepageBlur.setAttribute("value", blurLevel);
+            this.homepageBlur.setAttribute("progress", blurLevel);
+            this.homepageBlur.addEventListener("valueChange", ({ detail }) => {
+                pm.getPageByID("welcome").bgCanvas.style.filter = `blur(calc(${detail}px / var(--device-pixel-ratio)))`;
+                this.homepageBlur.setAttribute("progress", (+detail).toFixed(1));
+            });
+            this.homepageBlur.addEventListener("pointerdown", e => {
+                const x = this.homepageBlur.offsetLeft, y = this.homepageBlur.offsetTop;
+                const w = this.homepageBlur.offsetWidth, h = this.homepageBlur.offsetHeight;
+                this.style.clipPath = `polygon(${x}px ${y}px, ${x + w}px ${y}px, ${x + w}px ${y + h}px, ${x}px ${y + h}px)`;
+            });
+            for (let e of "pointerup,pointercancel,pointerout".split(","))
+                this.addEventListener(e, () => {
+                    this.style.clipPath = null;
+                });
+        }
+    };
+    onHistoryBack() { this.close(); };
+};
+
+SettingPage.asyncLoadAndDefine();
+
+
+export {
+    SettingPage,
+};
